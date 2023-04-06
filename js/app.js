c8e8fa6c9d99d67b092c553f598785e7439c233e
cipher-edu <ciphereduuz@gmail.com>
2023-04-06 09:52:08 +0500

    add

---
+++ js/app.js
@@ -0,0 +1,50 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+if (ScrollTrigger.isTouch !== 1) {
+
+	ScrollSmoother.create({
+		wrapper: '.wrapper',
+		content: '.content',
+		smooth: 1.5,
+		effects: true
+	})
+
+	gsap.fromTo('.hero-section', { opacity: 1 }, {
+		opacity: 0,
+		scrollTrigger: {
+			trigger: '.hero-section',
+			start: 'center',
+			end: '820',
+			scrub: true
+		}
+	})
+
+	let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
+
+	itemsL.forEach(item => {
+		gsap.fromTo(item, { opacity: 0, x: -50 }, {
+			opacity: 1, x: 0,
+			scrollTrigger: {
+				trigger: item,
+				start: '-850',
+				end: '-100',
+				scrub: true
+			}
+		})
+	})
+
+	let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+
+	itemsR.forEach(item => {
+		gsap.fromTo(item, { opacity: 0, x: 50 }, {
+			opacity: 1, x: 0,
+			scrollTrigger: {
+				trigger: item,
+				start: '-750',
+				end: 'top',
+				scrub: true
+			}
+		})
+	})
+
+}
